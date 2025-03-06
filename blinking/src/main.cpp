3dcf4871780401786a22390a05e09c66baaf4b9e
Alejandro Guaita <aguaitajimenez@hawk.iit.edu>
2025-03-05 19:07:08 -0600

    Fresh commit

--- blinking/src/main.cpp
+++ blinking/src/main.cpp
@@ -1,15 +1,23 @@
 #include <Arduino.h>
+#include <HardwareSerial.h>
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   int result = myFunction(2, 3);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("Hello World");
+  
+  digitalWrite(13, 1);
+  delay(1000);
+  digitalWrite(13, 0);
+  delay(1000);
 }
 
 // put function definitions here:
